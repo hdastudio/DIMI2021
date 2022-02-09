0615e9d90f57f7b479e2150f66621232e4c6688a
GlommerDeveloper <maksincognito@gmail.com>
2022-02-09 04:31:29 +0400

    1
    Begin

---
+++ src/main/java/com/nt/edu/fourthLab/FourthLabApplication.java
@@ -0,0 +1,13 @@
+package com.nt.edu.fourthLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourthLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourthLabApplication.class, args);
+	}
+
+}
