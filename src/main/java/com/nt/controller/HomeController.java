e04bf6c97791c9cbea8875c61df3f8f56de1911e
GlommerDeveloper <maksincognito@gmail.com>
2022-02-11 07:39:44 +0400

    Add display_detail.html and controllerFunc

--- src/main/java/com/nt/controller/HomeController.java
+++ src/main/java/com/nt/controller/HomeController.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class HomeController {
@@ -64,5 +67,14 @@ public class HomeController {
         Iterable<Task> tasks = taskRepo.findAll();
         model.put("tasks", tasks);
         return "display";
+    }
+
+    @GetMapping("/displayTasks/{id}")
+    public String displayTaskDetails(@PathVariable(value = "id") Long id, Model model){
+        Optional<Task> task = taskRepo.findById(id);
+        ArrayList<Task> result = new ArrayList<>();
+        task.ifPresent(result::add);
+        model.addAttribute("tasks", result);
+        return "display_detail";
     }
 }
